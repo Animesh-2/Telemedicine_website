b4ca404eeb0f15fbcb5438509b150d5503db82c0
Animesh Singh <animeshsingrol@gmail.com>
2023-11-30 14:06:54 +0530

    navbar done with mediaqueries

---
+++ navbar.js
@@ -0,0 +1,32 @@
+function navbar() {
+
+    return `
+    <ul class="navbar-left">
+        <a href="">
+            <li> Medicines </li>
+        </a>
+        <a href="">
+            <li> Doctor </li>
+        </a>
+        <a href="">
+            <li> Pharmacy </li>
+        </a>
+        <a href="">
+            <li> Other Services </li>
+        </a>
+    </ul>
+
+    <ul class="navbar-right">
+        <a href="">
+            <li><i class="fa-solid fa-landmark"></i> </li>
+        </a>
+        
+        <a href="">
+
+            <li>  Consult Now </li>
+        </a>
+    </ul>`
+
+}
+
+export {navbar}
